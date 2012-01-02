1aaa4914a205d3582625bdc89653bbf7b83b60b6
KOMATSU Issei <issei@mizuhataya.com>
2012-01-02 16:00:38 +0900

    hoge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorfil::Application.config.secret_token = 'b3baef0d97f18050ace9da8d650ccb254249f684ec04ddccf8eb3cb0a383d8bf1682dd86cd6f6eeb6f83db327cec6df1527124213d37822dd442fd82fdb34f09'
